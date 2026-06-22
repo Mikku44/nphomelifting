dd178eb55a136d6d106beccd0e1ec7504de153ca
Mikku44 <coverblack12345@gmail.com>
2026-06-22 18:04:23 +0700

    feat : fixed error deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import { netlifyPlugin } from "@netlify/vite-plugin"; // 1. Import the plugin
+import netlifyPlugin from "@netlify/vite-plugin"; // Fixed: Removed the curly braces
 
 export default defineConfig({
   plugins: [
     tailwindcss(), 
     reactRouter(), 
-    netlifyPlugin() // 2. Add it here
+    netlifyPlugin()
   ],
   resolve: {
     tsconfigPaths: true,
