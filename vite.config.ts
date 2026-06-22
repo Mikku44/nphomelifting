e781822335f97d6f8136023521277c7c6ce2382e
Mikku44 <coverblack12345@gmail.com>
2026-06-22 18:02:49 +0700

    feat : update netlify ssr compatible

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { netlifyPlugin } from "@netlify/vite-plugin"; // 1. Import the plugin
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(), 
+    reactRouter(), 
+    netlifyPlugin() // 2. Add it here
+  ],
   resolve: {
     tsconfigPaths: true,
   },
